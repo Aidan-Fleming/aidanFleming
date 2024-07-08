69f898e1999616ea096591e30506a4c1236e594d
Aidan-Fleming <155063909+Aidan-Fleming@users.noreply.github.com>
2024-07-08 09:18:22 +0100

    sad
    asd

---
+++ project2/libs/php/insertLocation.php
@@ -0,0 +1,80 @@
+<?php
+
+// example use from browser
+// http://localhost/companydirectory/libs/php/insertLocation.php?name=New%20Location
+
+// remove next two lines for production
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+$executionStartTime = microtime(true);
+
+// this includes the login details
+
+// include("config.php");
+
+$cd_host = "127.0.0.1";
+$cd_port = 3306;
+$cd_socket = "";
+
+// database name, username and password
+
+$cd_dbname = "companydirectory";
+$cd_user = 'root';
+$cd_password = '';
+
+header('Content-Type: application/json; charset=UTF-8');
+
+$conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
+
+if (mysqli_connect_errno()) {
+    
+    $output['status']['code'] = "300";
+    $output['status']['name'] = "failure";
+    $output['status']['description'] = "database unavailable";
+    $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+    $output['data'] = [];
+
+    mysqli_close($conn);
+
+    echo json_encode($output);
+
+    exit;
+
+}
+
+// SQL statement accepts parameters and so is prepared to avoid SQL injection.
+// $_REQUEST used for development / debugging. Remember to change to $_POST for production
+
+$query = $conn->prepare('INSERT INTO location (name) VALUES(?)');
+
+$query->bind_param("s", $_POST['name']);
+
+$query->execute();
+
+if ($query === false) {
+    $output['status']['code'] = "400";
+    $output['status']['name'] = "executed";
+    $output['status']['description'] = "query failed";    
+    $output['data'] = [];
+
+    mysqli_close($conn);
+
+    echo json_encode($output); 
+
+    exit;
+
+}
+
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
+$output['status']['description'] = "success";
+$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+$output['data'] = [];
+
+mysqli_close($conn);
+
+echo json_encode($output); 
+
+?>
